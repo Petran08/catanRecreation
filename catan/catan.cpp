0b3c4526d9c5ceb3998fd6f8367c50226ea83104
Raul Petran <petranraulfrancesco@gmail.com>
2026-02-15 23:37:04 +0200

    i m bored

--- catan/catan.cpp
+++ catan/catan.cpp
@@ -5,19 +5,26 @@
 
 int screenWidth = 1920, screenHeight = 1080;
 
+Vector2 pos;
+
 int main()
 {
     bool mat[54][54];
 
     InitWindow(screenWidth, screenHeight - 95, "catan");
 
+    SetTargetFPS(120);
+
     while(!WindowShouldClose())
     {
+        pos.x += (IsKeyDown(KEY_RIGHT) - IsKeyDown(KEY_LEFT));
+        pos.y += (IsKeyDown(KEY_DOWN) - IsKeyDown(KEY_UP));
+
         BeginDrawing();
 
         ClearBackground(WHITE);
 
-        drawHexagon(Vector2{ 500, 400 }, HEX_SIZE, GREEN);
+        drawHexagon(pos, HEX_SIZE, GREEN);
 
         EndDrawing();
     }
